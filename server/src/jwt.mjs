09c322daa88a5c878038a3d8a2d1fa6bf89c4aca
Douglas Barone <douglasvbarone@gmail.com>
2023-02-16 08:49:54 -0400

    Remove log

--- server/src/jwt.mjs
+++ server/src/jwt.mjs
@@ -11,8 +11,6 @@ export function generateToken(username) {
 }
 
 export function validateToken(authorization) {
-  console.log({ authorization })
-
   if (!authorization) throw new Error('Missing authorization header')
 
   const [scheme, token] = authorization.split(' ')
